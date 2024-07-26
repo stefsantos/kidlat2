7ef8204bd56691807de037bee49b6d611949ce11
alypoopoo <alisha_chua@dlsu.edu.ph>
2024-07-26 22:18:45 +0800

    fixed login layout + added assets

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/widgets.dart';
 import 'package:kidlat/pages/SignUpPages/SignUp.dart';
 import 'package:kidlat/pages/auth_page.dart';
 
-
 class LoginPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
@@ -30,9 +29,11 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         automaticallyImplyLeading: true, 
-        title: const Text('Login'),
+        backgroundColor: Colors.white,
+        elevation: 0,        
         leading: IconButton(
           color: Colors.black,
           icon: const Icon(Icons.arrow_back),
@@ -40,151 +41,162 @@ class LoginPage extends StatelessWidget {
             Navigator.pushReplacementNamed(context, '/');
           },
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.all(20.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Image.asset(
+              "assets/3.png",
+              height: 150.0,
+              fit: BoxFit.cover,
+            ),
           ),
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: Container(
-                  width: 73,
-                  height: 61,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage("https://via.placeholder.com/73x61"),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height:20),
-              Container(
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Container(
                 width: double.infinity,
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 40,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: -0.30,
-                    ),
-                  )
-                )
-              ),
-              // email textfield
-              SizedBox(height: 20),
-              _buildInputField('Email Address', emailController),
-              // password textfield
-              SizedBox(height: 10),
-              _buildInputField('Password', passwordController, obscureText: true),
-              SizedBox(height: 30),
-              TextButton(
-                onPressed: () {
-                    print("Proceed to Login Button tapped!");
-                    signUserIn(context);  // Call signUserIn method
-                },
-                child: Text(
-                  'Login',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-                ),
-                style: TextButton.styleFrom(
-                  elevation: 2,
-                  backgroundColor: Colors.white,
-                  minimumSize: Size(350, 48.43),
-                  shadowColor: Colors.black.withOpacity(0.2),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    side: BorderSide(
-                      color: Colors.grey.shade600.withOpacity(0.2),
-                      width: 2,
-                    )
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                width: 318,
-                height: 1.15,
+                padding: const EdgeInsets.all(20.0),
                 decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.2),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              ),
-              SizedBox(height: 30),
-              Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacementNamed(context, '/sign_up');
-                    },
-                    child: SizedBox(
-                      width: 198,
-                      height: 21,
-                      child: Text.rich(
-                        TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'Don’t have an account? ',
-                              style: TextStyle(
-                                color: Colors.black.withOpacity(0.7),
-                                fontSize: 11,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w400,
-                                height: 0.15,
-                                letterSpacing: -0.08,
-                              ),
-                            ),
-                            TextSpan(
-                              text: 'Sign up here',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 11,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w700,
-                                height: 0.15,
-                                letterSpacing: -0.08,
-                              ),
-                            ),
-                          ],
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    Container(
+                      width: double.infinity,
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 40,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: -0.30,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Spacer(),
-                  SizedBox(
-                    width: 118,
-                    height: 21,
-                    child: Text(
-                      'Forgot password?',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFFFEB62C),
-                        fontSize: 11,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w600,
-                        height: 0.15,
-                        letterSpacing: -0.08,
+                    // email textfield
+                    SizedBox(height: 20),
+                    _buildInputField('Email Address', emailController),
+                    // password textfield
+                    SizedBox(height: 10),
+                    _buildInputField('Password', passwordController, obscureText: true),
+                    SizedBox(height: 30),
+                    TextButton(
+                      onPressed: () {
+                        print("Proceed to Login Button tapped!");
+                        signUserIn(context);  // Call signUserIn method
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        elevation: 2,
+                        backgroundColor: Colors.white,
+                        minimumSize: Size(350, 48.43),
+                        shadowColor: Colors.black.withOpacity(0.2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          side: BorderSide(
+                            color: Colors.grey.shade600.withOpacity(0.2),
+                            width: 2,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 30),
+                    Container(
+                      width: 318,
+                      height: 1.15,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.2),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacementNamed(context, '/sign_up');
+                          },
+                          child: SizedBox(
+                            width: 198,
+                            height: 21,
+                            child: Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: 'Don’t have an account? ',
+                                    style: TextStyle(
+                                      color: Colors.black.withOpacity(0.7),
+                                      fontSize: 11,
+                                      fontFamily: 'Poppins',
+                                      fontWeight: FontWeight.w400,
+                                      height: 0.15,
+                                      letterSpacing: -0.08,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: 'Sign up here',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 11,
+                                      fontFamily: 'Poppins',
+                                      fontWeight: FontWeight.w700,
+                                      height: 0.15,
+                                      letterSpacing: -0.08,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        SizedBox(
+                          width: 118,
+                          height: 21,
+                          child: Text(
+                            'Forgot password?',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color(0xFFFEB62C),
+                              fontSize: 11,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                              height: 0.15,
+                              letterSpacing: -0.08,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ],
+            ),
           ),
-        ),
+          Container(
+            width: double.infinity,
+            child: Image.asset(
+              'assets/loginpassword.png',
+              fit: BoxFit.cover
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -220,3 +232,4 @@ Widget _buildInputField(String hintText, TextEditingController controller, {bool
     ],
   );
 }
+
